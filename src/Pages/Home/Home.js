6ca707ee5386f1e112d2f48ed0c671fa9524678e
Sharif101 <sharif009009@gmail.com>
2022-11-08 19:11:12 +0600

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,30 +13,33 @@ const Home = () => {
         <Carousel.Item className="slide">
           <img className="d-block w-100" src={pic1} alt="First slide" />
           <Carousel.Caption className="slide-text">
-            <h3>Study Buzz</h3>
+            <h3>Shutter Up</h3>
             <p>
-              The authority of those who teach is often an obstacle to those who
-              want to learn.
+              If your photos aren’t good enough, then you’re not close enough.
             </p>
-            <p>― Marcus Tullius Cicero</p>
+            <p>— Robert Capa</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="slide">
           <img className="d-block w-100" src={pic2} alt="Second slide" />
 
           <Carousel.Caption className="slide-text">
-            <h3>Study Buzz</h3>
-            <p>You can't study the darkness by flooding it with light.</p>
-            <p>― Edward Abbey,</p>
+            <h3>Shutter Up</h3>
+            <p>
+              When words become unclear, I shall focus with photographs. When
+              images become inadequate, I shall be content with silence.
+            </p>
+            <p>— Ansel Adams</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item className="slide">
           <img className="d-block w-100" src={pic3} alt="Third slide" />
 
           <Carousel.Caption className="slide-text">
-            <h3>Study Buzz</h3>
+            <h3>Shutter Up</h3>
             <p>
-              The ink of the scholar is more holy than the blood of the martyr.
+              In photography there is a reality so subtle that it becomes more
+              real than reality.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div>
-      <h2>Footer</h2>
+      <footer className="footer">
+        <ul class="icons">
+          <li>
+            <a href="#">
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+        <ul class="menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+        <div class="footer-copyright">
+          <p>Copyright @ 2022 All Rights Reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 };
